5c4ea541e921e9d6f404ec88b78972812705b230
bvisvanathan <balaji.visvanathan@gmail.com>
2021-10-25 11:43:40 +0530

    first commit 20211025

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+
+@NgModule({
+  declarations: [],
+  imports: [MatSliderModule],
+  exports: [MatSliderModule],
+})
+export class AppMaterialModule {}
